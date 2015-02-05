afaf8c4542faf571a3cbff3e32064223ee862a77
axton21 <axton_n@yahoo.com>
2015-02-05 10:46:29 -0500

    let it go

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+
+def alphabetize(array)
+    esperanto = "abcĉdefgĝhĥijĵklmnopqrsŝtuŭvxywz".split("")
+    newarray = []
+    array.sort!
+    esperanto.each do |char| # abcde....
+      array.each do |string| #["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
+        if char == string[0]
+            newarray << string
+              # binding.pry
+        end
+      end
+    end
+  newarray
 end
+#abcĉdefgĝhĥijĵklmnoprsŝtuŭvz
